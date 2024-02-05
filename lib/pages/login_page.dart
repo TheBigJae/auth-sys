b21dd79bf57a3e27c67c469ca2ea4701560f44a4
TheBigJae <johnbamigbola10@gmail.com>
2024-02-05 18:33:06 +0100

    Adding Images

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -72,10 +72,38 @@ class LoginPage extends StatelessWidget {
             //Sign In Button
             MyButton(onTap: signUserIn,),
 
-            SizedBox(height: 50,)
+            SizedBox(height: 50,),
             //Or continue with
-
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal:25.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal:10.0),
+                    child: Text('Or continnue with', style: TextStyle(color: Colors.grey[700]),),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ],
+              ),
+            ),
             // Google Sign In Button
+            Row(children: [
+               //google button 
+            
+            // apple button
+            ],)
+           
           ],
         ),
       )),
